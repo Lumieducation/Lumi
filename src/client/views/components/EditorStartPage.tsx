234b8b83a7fd933d9e21a555e99799342f4729af
Sebastian Rettig <sr258@users.noreply.github.com>
2020-11-22 17:44:18 +0100

    refactor(ui): removed unused import

--- src/client/views/components/EditorStartPage.tsx
+++ src/client/views/components/EditorStartPage.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 
-import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
