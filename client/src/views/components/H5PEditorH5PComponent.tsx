ea3f10cf265243bb276c8240ff26fdeab539ffb1
JPSchellenberg <jps@Lumi.education>
2021-01-03 19:13:02 +0100

    feat(htmlexport): add UI elements

--- client/src/views/components/H5PEditorH5PComponent.tsx
+++ client/src/views/components/H5PEditorH5PComponent.tsx
@@ -13,6 +13,7 @@ import { createStyles, Theme, withStyles } from '@material-ui/core/styles';
 import { H5PPlayerUI, H5PEditorUI } from '@lumieducation/h5p-react';
 
 import SaveButton from './SaveButton';
+import ExportAsHtmlButton from './ExportAsHtmlButton';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 
@@ -136,6 +137,10 @@ export class H5PEditorH5PComponent extends React.Component<IH5PEditorH5PComponen
                         </div>
                     </ContentPaper>
                 </Grid>
+                <ExportAsHtmlButton
+                    onClick={this.exportAsHtml}
+                    state={this.props.tab.exportButtonState}
+                />
                 <SaveButton
                     onClick={this.export}
                     state={this.props.tab.saveButtonState}
