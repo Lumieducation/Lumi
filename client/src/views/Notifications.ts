fddf7950ffc05ed814c0d563a8d7e85f702bd27f
Jan Philip Schellenberg <jps@Lumi.education>
2021-01-07 08:00:10 +0100

    feat(notifications): reenable notifications (#1135)
    * feat(notifications): reenable notifications
    
    * feat(notifications): add notifications for export & exportHtml actions
    
    * fix(notifications): remove duplicate case

--- client/src/views/Notifications.ts
+++ client/src/views/Notifications.ts
@@ -1,23 +1,24 @@
-// import { withSnackbar } from 'notistack';
+import { withSnackbar } from 'notistack';
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-// import { INotification } from '../state/Notifications/NotificationsTypes';
+import { INotification } from '../state/Notifications/NotificationsTypes';
 
-// import { actions, selectors } from '../state/Notifications';
+import * as actions from '../state/Notifications/NotificationsActions';
+import * as selectors from '../state/Notifications/NotificationsSelectors';
 
 interface IPassedProps {
-    // enqueueSnackbar: (message: string, options: any) => void;
+    enqueueSnackbar: (message: string, options: any) => void;
 }
 
 interface IStateProps extends IPassedProps {
-    // notifications: INotification[];
+    notifications: INotification[];
 }
 
 interface IDispatchProps {
-    // closeSnackbar: typeof actions.closeSnackbar;
-    // removeSnackbar: typeof actions.removeSnackbar;
+    closeSnackbar: typeof actions.closeSnackbar;
+    removeSnackbar: typeof actions.removeSnackbar;
 }
 
 interface IComponentState {}
@@ -31,24 +32,25 @@ export class NotificationsContainer extends React.Component<
     private displayed: string[] = [];
 
     public componentDidUpdate(): void {
-        // const { notifications = [] } = this.props;
-        // notifications.forEach(({ key, message, options = {} }) => {
-        // Do nothing if snackbar is already displayed
-        // if (this.displayed.includes(key)) return;
-        // Display snackbar using notistack
-        // this.props.enqueueSnackbar(message, {
-        //     ...options,
-        //     onClose: (event: any, reason: any, key2: string) => {
-        //         // if (options.onClose) {
-        //         //     options.onClose(event, reason, key);
-        //         // }
-        //         // // Dispatch action to remove snackbar from redux store
-        //         this.props.removeSnackbar(key2);
-        //     }
-        // });
-        // Keep track of snackbars that we've displayed
-        // this.storeDisplayed(key);
-        // });
+        const { notifications = [] } = this.props;
+
+        notifications.forEach(({ key, message, options = {} }) => {
+            // Do nothing if snackbar is already displayed
+            if (this.displayed.includes(key)) return;
+            // Display snackbar using notistack
+            this.props.enqueueSnackbar(message, {
+                ...options,
+                onClose: (event: any, reason: any, key2: string) => {
+                    // if (options.onClose) {
+                    //     options.onClose(event, reason, key);
+                    // }
+                    // // Dispatch action to remove snackbar from redux store
+                    this.props.removeSnackbar(key2);
+                }
+            });
+            // Keep track of snackbars that we've displayed
+            this.storeDisplayed(key);
+        });
     }
 
     public render(): null {
@@ -63,21 +65,21 @@ export class NotificationsContainer extends React.Component<
             return false;
         }
 
-        // const { notifications: currentSnacks } = this.props;
+        const { notifications: currentSnacks } = this.props;
         let notExists = false;
         // tslint:disable-next-line: prefer-for-of
-        // for (let i = 0; i < newSnacks.length; i += 1) {
-        //     const newSnack = newSnacks[i];
-        //     if (newSnack.dismissed) {
-        //         // this.props.closeSnackbar(newSnack.key);
-        //         // this.props.removeSnackbar(newSnack.key);
-        //     }
+        for (let i = 0; i < newSnacks.length; i += 1) {
+            const newSnack = newSnacks[i];
+            if (newSnack.dismissed) {
+                this.props.closeSnackbar(newSnack.key);
+                this.props.removeSnackbar(newSnack.key);
+            }
 
-        //     if (notExists) continue;
-        //     notExists =
-        //         notExists ||
-        //         !currentSnacks.filter(({ key }) => newSnack.key === key).length;
-        // }
+            if (notExists) continue;
+            notExists =
+                notExists ||
+                !currentSnacks.filter(({ key }) => newSnack.key === key).length;
+        }
         return notExists;
     }
 
@@ -88,21 +90,20 @@ export class NotificationsContainer extends React.Component<
 
 function mapStateToProps(state: any, ownProps: IPassedProps): IStateProps {
     return {
-        // enqueueSnackbar: ownProps.enqueueSnackbar,
-        // notifications: selectors.notifications(state)
+        enqueueSnackbar: ownProps.enqueueSnackbar,
+        notifications: selectors.notifications(state)
     };
 }
 
 function mapDispatchToProps(dispatch: any): IDispatchProps {
     return bindActionCreators(
         {
-            // closeSnackbar: actions.closeSnackbar,
-            // removeSnackbar: actions.removeSnackbar
+            closeSnackbar: actions.closeSnackbar,
+            removeSnackbar: actions.removeSnackbar
         },
         dispatch
     );
 }
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(NotificationsContainer);
+export default withSnackbar(
+    connect(mapStateToProps, mapDispatchToProps)(NotificationsContainer)
+);
