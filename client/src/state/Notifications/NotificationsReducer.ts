1e95e3cc0e08de2e9ac641187769fced6e4618a9
Jan Philip Schellenberg <jps@Lumi.education>
2021-01-07 08:00:10 +0100

    feat(notifications): reenable notifications (#1135)
    * feat(notifications): reenable notifications
    
    * feat(notifications): add notifications for export & exportHtml actions
    
    * fix(notifications): remove duplicate case

--- client/src/state/Notifications/NotificationsReducer.ts
+++ client/src/state/Notifications/NotificationsReducer.ts
@@ -2,9 +2,23 @@ import {
     CLOSE_SNACKBAR,
     ENQUEUE_SNACKBAR,
     INotificationsState,
-    NotificationActionTypes,
-    REMOVE_SNACKBAR
+    REMOVE_SNACKBAR,
+    INotifyAction,
+    ICloseSnackbar,
+    IRemoveSnackbar
 } from './NotificationsTypes';
+
+import {
+    H5P_EXPORT_SUCCESS,
+    H5P_EXPORT_ERROR,
+    H5PEDITOR_EXPORTHTML_SUCCESS,
+    H5PEDITOR_EXPORTHTML_ERROR,
+    IH5PExportSuccessAction,
+    IH5PExportErrorAction,
+    IH5PEditorExportHtmlErrorAction,
+    IH5PEditorExportHtmlSuccessAction
+} from '../H5PEditor/H5PEditorTypes';
+import shortid from 'shortid';
 
 export const initialState: INotificationsState = {
     notifications: []
@@ -12,9 +26,76 @@ export const initialState: INotificationsState = {
 
 export default function notificationsReducer(
     state: INotificationsState = initialState,
-    action: NotificationActionTypes
+    action:
+        | INotifyAction
+        | ICloseSnackbar
+        | IRemoveSnackbar
+        | IH5PExportSuccessAction
+        | IH5PExportErrorAction
+        | IH5PEditorExportHtmlErrorAction
+        | IH5PEditorExportHtmlSuccessAction
 ): INotificationsState {
     switch (action.type) {
+        case H5P_EXPORT_SUCCESS:
+            return {
+                ...state,
+                notifications: [
+                    ...state.notifications,
+                    {
+                        key: shortid(),
+                        message: 'notification.export.success',
+                        options: {
+                            variant: 'success'
+                        }
+                    }
+                ]
+            };
+
+        case H5P_EXPORT_ERROR:
+            return {
+                ...state,
+                notifications: [
+                    ...state.notifications,
+                    {
+                        key: shortid(),
+                        message: 'notification.export.error',
+                        options: {
+                            variant: 'error'
+                        }
+                    }
+                ]
+            };
+
+        case H5PEDITOR_EXPORTHTML_SUCCESS:
+            return {
+                ...state,
+                notifications: [
+                    ...state.notifications,
+                    {
+                        key: shortid(),
+                        message: 'notification.exporthtml.success',
+                        options: {
+                            variant: 'success'
+                        }
+                    }
+                ]
+            };
+
+        case H5PEDITOR_EXPORTHTML_ERROR:
+            return {
+                ...state,
+                notifications: [
+                    ...state.notifications,
+                    {
+                        key: shortid(),
+                        message: 'notification.exporthtml.error',
+                        options: {
+                            variant: 'error'
+                        }
+                    }
+                ]
+            };
+
         case ENQUEUE_SNACKBAR:
             return {
                 ...state,
