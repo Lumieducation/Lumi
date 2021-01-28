7c31dd958b9da127023c7c1e8adc77a8faeae2a0
JPSchellenberg <jps@Lumi.education>
2021-01-28 15:54:36 +0100

    fix(reporter): add padding to content

--- server/src/routes/h5pRoutes.ts
+++ server/src/routes/h5pRoutes.ts
@@ -85,7 +85,7 @@ export default function (
             <body>
             <div id="root"></div>
             ${createReporter(reporterClient, reporterMain)}
-                <div style="margin: 20px auto; max-width: 840px; box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)" class="h5p-content lag" data-content-id="${contentId}"></div>                
+                <div style="margin: 20px auto; padding: 20px;  max-width: 840px; box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)" class="h5p-content lag" data-content-id="${contentId}"></div>                
             </body>
             </html>`
                 : undefined
