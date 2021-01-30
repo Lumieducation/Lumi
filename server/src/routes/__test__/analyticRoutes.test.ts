4ff0215e2fd30c3b13df60155c269b95cd2d86c1
JPSchellenberg <jps@Lumi.education>
2021-01-30 01:29:21 +0100

    fix(analytics): allow file + multiple selection when importing analytic

--- server/src/routes/__test__/analyticRoutes.test.ts
+++ server/src/routes/__test__/analyticRoutes.test.ts
@@ -35,6 +35,85 @@ describe('[analytics:routes]: GET /api/v1/analytics', () => {
                     name: 'test',
                     id: '43ce1094-a6e0-43f8-9368-5aac8352c357',
                     results: [1]
+                },
+                {
+                    id: '43ce1094-a6e0-43f8-9368-5aac8352c357',
+                    name: 'test2',
+                    results: [1]
+                }
+            ]
+        });
+        done();
+    });
+
+    it('should return the correct data when a file is selected', async (done) => {
+        dialog.showOpenDialog = jest.fn(async (c) => {
+            return {
+                canceled: false,
+                filePaths: [
+                    path.resolve(
+                        'test',
+                        'data',
+                        'analytics',
+                        'valid',
+                        'test.lumi'
+                    )
+                ]
+            };
+        });
+
+        const res = await request(app).get('/api/v1/analytics');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toStrictEqual({
+            interactions: [{ name: 'MultiChoice', id: 'skip' }],
+            users: [
+                {
+                    name: 'test',
+                    id: '43ce1094-a6e0-43f8-9368-5aac8352c357',
+                    results: [1]
+                }
+            ]
+        });
+        done();
+    });
+
+    it('should return the correct data when multiple files are selected', async (done) => {
+        dialog.showOpenDialog = jest.fn(async (c) => {
+            return {
+                canceled: false,
+                filePaths: [
+                    path.resolve(
+                        'test',
+                        'data',
+                        'analytics',
+                        'valid',
+                        'test.lumi'
+                    ),
+                    path.resolve(
+                        'test',
+                        'data',
+                        'analytics',
+                        'valid',
+                        'test2.lumi'
+                    )
+                ]
+            };
+        });
+
+        const res = await request(app).get('/api/v1/analytics');
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toStrictEqual({
+            interactions: [{ name: 'MultiChoice', id: 'skip' }],
+            users: [
+                {
+                    name: 'test',
+                    id: '43ce1094-a6e0-43f8-9368-5aac8352c357',
+                    results: [1]
+                },
+                {
+                    id: '43ce1094-a6e0-43f8-9368-5aac8352c357',
+                    name: 'test2',
+                    results: [1]
                 }
             ]
         });
@@ -96,6 +175,20 @@ describe('[analytics:routes]: GET /api/v1/analytics', () => {
                         'different_content'
                     )
                 ]
+            };
+        });
+
+        const res = await request(app).get('/api/v1/analytics');
+        expect(res.statusCode).toEqual(400);
+
+        done();
+    });
+
+    it('should return 400 if imported data is from different contents', async (done) => {
+        dialog.showOpenDialog = jest.fn(async (c) => {
+            return {
+                canceled: false,
+                filePaths: [path.resolve('test', 'data', 'analytics')]
             };
         });
 
--- server/src/routes/analyticRoutes.ts
+++ server/src/routes/analyticRoutes.ts
@@ -24,92 +24,94 @@ export default function (): express.Router {
                     name: 'Lumi Report'
                 }
             ],
-            properties: ['openDirectory']
+            properties: ['openDirectory', 'openFile', 'multiSelections']
         });
 
         if (openDialog.canceled) {
             return res.status(499).end();
         }
 
+        let files: string[] = [];
+
+        await Promise.all(
+            openDialog.filePaths.map(async (filePath) => {
+                if (_path.extname(filePath) === '.lumi') {
+                    files.push(filePath);
+                }
+                if (_path.extname(filePath) === '') {
+                    const f = await recursiveReaddir(filePath, ['!*.lumi']);
+                    files = [...files, ...f];
+                }
+            })
+        );
+
         try {
-            recursiveReaddir(
-                openDialog.filePaths[0],
-                ['!*.lumi'],
-                (err, files) => {
-                    if (err) {
-                        return res.status(500).json(err);
-                    }
-                    if (files.length === 0) {
-                        return res.status(404).end();
-                    }
-                    const userStatements = {};
-                    let contentJson;
-                    let library: string;
-                    let error: boolean = false;
+            if (files.length === 0) {
+                return res.status(404).end();
+            }
+            const userStatements = {};
+            let contentJson;
+            let library: string;
+            let error: boolean = false;
 
-                    files.forEach((f) => {
-                        const d = JSON.parse(
-                            fs.readFileSync(f, { encoding: 'utf-8' })
-                        );
+            files.forEach((f) => {
+                const d = JSON.parse(fs.readFileSync(f, { encoding: 'utf-8' }));
 
-                        userStatements[_path.basename(f, '.lumi')] = d.xapi;
+                userStatements[_path.basename(f, '.lumi')] = d.xapi;
 
-                        if (contentJson && d.contentJson) {
-                            if (
-                                objectHash(contentJson) !==
-                                objectHash(d.contentJson)
-                            ) {
-                                error = true;
-                                res.status(400).json({
-                                    message: `${f} is from a different content`
-                                });
-                            }
-                        }
-
-                        contentJson = d.contentJson;
-                        library = d.library;
-                    });
-
-                    if (error) {
-                        return;
-                    }
-                    const interactions: IInteraction[] = [];
-
-                    try {
-                        getInteractions(contentJson, interactions);
-                    } catch (error) {
-                        return res.status(500).json(error);
-                    }
-
-                    if (interactions.length === 0) {
-                        interactions.push({
-                            name: library.replace('H5P.', '').split(' ')[0],
-                            id: 'skip'
+                if (contentJson && d.contentJson) {
+                    if (objectHash(contentJson) !== objectHash(d.contentJson)) {
+                        error = true;
+                        res.status(400).json({
+                            message: `${f} is from a different content`
                         });
                     }
-
-                    const users = Object.keys(userStatements).map((key) => {
-                        return {
-                            name: key,
-                            id: userStatements[key][0].actor.account.name,
-                            results: interactions.map(
-                                (interaction) =>
-                                    getResult(
-                                        userStatements[key],
-                                        interaction.id === 'skip'
-                                            ? undefined
-                                            : interaction.id
-                                    ).score.scaled
-                            )
-                        };
-                    });
-
-                    res.status(200).json({
-                        interactions,
-                        users
-                    });
                 }
-            );
+
+                contentJson = d.contentJson;
+                library = d.library;
+            });
+
+            if (error) {
+                return;
+            }
+            const interactions: IInteraction[] = [];
+
+            try {
+                getInteractions(contentJson, interactions);
+            } catch (error) {
+                return res.status(500).json(error);
+            }
+
+            if (interactions.length === 0) {
+                interactions.push({
+                    name: library.replace('H5P.', '').split(' ')[0],
+                    id: 'skip'
+                });
+            }
+
+            const users = Object.keys(userStatements).map((key) => {
+                return {
+                    name: key,
+                    id: userStatements[key][0].actor.account.name,
+                    results: interactions.map(
+                        (interaction) =>
+                            getResult(
+                                userStatements[key],
+                                interaction.id === 'skip'
+                                    ? undefined
+                                    : interaction.id
+                            ).score.scaled
+                    )
+                };
+            });
+
+            res.status(200).json({
+                interactions,
+                users
+            });
+            //     }
+            // );
         } catch (error) {
             res.status(500).json(error);
         }
