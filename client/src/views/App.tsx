72c6bc1f6423cb70eb521dfe8109b9b752dd9b90
Jan Philip Schellenberg <jps@Lumi.education>
2021-02-01 10:35:16 +0100

    fix(ui): display a backbutton if not in launchpad (#1209)
    * fix(deps): update h5p-server to v7.0.0-alpha.1
    
    * fix(ui): display a backbutton when not in launchpad

--- client/src/views/App.tsx
+++ client/src/views/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { bindActionCreators } from 'redux';
 
 import Logger from '../helpers/Logger';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { createStyles, Theme, withStyles } from '@material-ui/core/styles';
 
 import AppBar from './components/AppBar';
 
@@ -18,71 +15,31 @@ import Launchpad from './Launchpad';
 
 import Websocket from './Websocket';
 
-import { IState } from '../state';
-
 const log = new Logger('container:app');
 
-interface IPassedProps {
-    classes: any;
+export default function AppContaine() {
+    log.info(`rendering`);
+    return (
+        <div id="app">
+            <CssBaseline />
+            <Websocket />
+            <Router>
+                <AppBar />
+                <Switch>
+                    <Route
+                        exact={true}
+                        path="/h5peditor"
+                        component={H5PEditor}
+                    />
+                    <Route
+                        exact={true}
+                        path="/analytics"
+                        component={Analytics}
+                    />
+                    <Route path="/" component={Launchpad} />
+                </Switch>
+            </Router>
+            <Notifications />
+        </div>
+    );
 }
-
-interface IStateProps extends IPassedProps {}
-
-interface IDispatchProps {}
-
-interface IComponentState {}
-
-interface IProps extends IStateProps, IDispatchProps {}
-
-export class AppContainer extends React.Component<IProps, IComponentState> {
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {};
-    }
-
-    public render(): JSX.Element {
-        log.info(`rendering`);
-        return (
-            <div id="app">
-                <CssBaseline />
-                <Websocket />
-                <Router>
-                    <AppBar />
-                    <Switch>
-                        <Route
-                            exact={true}
-                            path="/h5peditor"
-                            component={H5PEditor}
-                        />
-                        <Route
-                            exact={true}
-                            path="/analytics"
-                            component={Analytics}
-                        />
-                        <Route path="/" component={Launchpad} />
-                    </Switch>
-                </Router>
-                <Notifications />
-            </div>
-        );
-    }
-}
-
-const styles = (theme: Theme) => createStyles({});
-function mapStateToProps(state: IState, ownProps: IPassedProps): IStateProps {
-    return {
-        classes: ownProps.classes
-    };
-}
-
-function mapDispatchToProps(dispatch: any): IDispatchProps {
-    return bindActionCreators({}, dispatch);
-}
-
-export default withStyles(styles)(
-    connect<IStateProps, IDispatchProps, IPassedProps, IState>(
-        mapStateToProps,
-        mapDispatchToProps
-    )(AppContainer)
-);
