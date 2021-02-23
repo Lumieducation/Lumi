55c4a81a229a3d2d916dad0f9c1b2df220d158ad
JPSchellenberg <jps@Lumi.education>
2021-02-24 00:50:54 +0100

    test(lint): make linter happy

--- client/src/views/components/BugReportSettings.tsx
+++ client/src/views/components/BugReportSettings.tsx
@@ -7,7 +7,6 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 import BugReportIcon from '@material-ui/icons/BugReport';
