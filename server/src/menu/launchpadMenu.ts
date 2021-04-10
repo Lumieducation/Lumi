961abbf760cb31374d77410d4572dc7e4f3fc4df
JPSchellenberg <jps@Lumi.education>
2021-04-11 01:02:39 +0200

    fix(mac-menu): missing default mac-menu

--- server/src/menu/launchpadMenu.ts
+++ server/src/menu/launchpadMenu.ts
@@ -2,9 +2,11 @@ import electron from 'electron';
 import SocketIO from 'socket.io';
 import editMenu from './editMenu';
 
+import macMenu from './macMenu';
 import helpMenu from './helpMenu';
 
 export default (window: electron.BrowserWindow, websocket: SocketIO.Server) => [
+    ...macMenu(),
     editMenu(),
     helpMenu(window, websocket)
 ];
