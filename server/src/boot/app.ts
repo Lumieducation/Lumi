5bf515260b596b2d19b4f2caa32b18b8a28d2655
JPSchellenberg <jps@Lumi.education>
2021-04-12 13:02:13 +0200

    fix(sentry): add new DSN

--- server/src/boot/app.ts
+++ server/src/boot/app.ts
@@ -82,7 +82,7 @@ export default async (
     if (process.env.NODE_ENV !== 'development') {
         Sentry.init({
             dsn:
-                'http://1f4ae874b81a48ed8e22fe6e9d52ed1b@sentry.lumi.education/3',
+                'https://c9daaaa874704f15b172ada7ebf44c87@sen.lumi.education/2',
             release: electron.app.getVersion(),
             environment: process.env.NODE_ENV,
             beforeSend: async (event: Sentry.Event) => {
