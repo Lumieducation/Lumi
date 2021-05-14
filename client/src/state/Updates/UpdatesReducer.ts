021c6d0cf02847a268a0058a079be1003fcb1842
JPSchellenberg <jps@Lumi.education>
2021-05-15 00:34:43 +0200

    feat(updates): display update details and add update and restart button

---
+++ client/src/state/Updates/UpdatesReducer.ts
@@ -0,0 +1,45 @@
+import * as Sentry from '@sentry/browser';
+
+import {
+    IUpdatesState,
+    IUpdatesActionTypes,
+    UPDATES_GET_UPDATES_SUCCESS
+} from './UpdatesTypes';
+
+export const initialState: IUpdatesState = {
+    updateAvailable: false,
+    checkingForUpdates: false,
+    updateInfo: {
+        version: '',
+        releaseName: '',
+        releaseNotes: '',
+        releaseDate: ''
+    },
+    downloadProgress: {
+        progress: 0,
+        bytesPersecond: 0,
+        percent: 0,
+        total: 0,
+        transferred: 0
+    }
+};
+
+export default function updatesReducer(
+    state: IUpdatesState = initialState,
+    action: IUpdatesActionTypes
+): IUpdatesState {
+    try {
+        switch (action.type) {
+            case UPDATES_GET_UPDATES_SUCCESS:
+                return {
+                    ...state,
+                    updateInfo: action.payload
+                };
+            default:
+                return state;
+        }
+    } catch (error) {
+        Sentry.captureException(error);
+        return state;
+    }
+}
