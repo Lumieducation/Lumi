03fe6063b329d867f25d58b50f226bd33c869a25
Sebastian Rettig <serettig@posteo.de>
2021-07-09 18:21:50 +0200

    refactor(run): moved run menu entry

--- server/src/menu/h5peditorMenu.ts
+++ server/src/menu/h5peditorMenu.ts
@@ -10,87 +10,96 @@ import viewMenu from './viewMenu';
 import runMenu from './runMenuItem';
 import settings from '../settingsCache';
 
-export default (window: electron.BrowserWindow, websocket: SocketIO.Server) =>
-    [
-        ...macMenu(),
-        {
-            label: i18next.t('lumi:menu.file.label'),
-            submenu: [
-                {
-                    accelerator: 'CmdOrCtrl+N',
-                    click: () => {
-                        websocket.emit('action', {
-                            payload: {
-                                contentId: Math.round(Math.random() * 100000)
-                            },
-                            type: 'NEW_H5P'
-                        });
-                    },
-                    label: i18next.t('lumi:menu.h5peditor.new')
+export default (window: electron.BrowserWindow, websocket: SocketIO.Server) => [
+    ...macMenu(),
+    {
+        label: i18next.t('lumi:menu.file.label'),
+        submenu: [
+            {
+                accelerator: 'CmdOrCtrl+N',
+                click: () => {
+                    websocket.emit('action', {
+                        payload: {
+                            contentId: Math.round(Math.random() * 100000)
+                        },
+                        type: 'NEW_H5P'
+                    });
                 },
-                { type: 'separator' } as any,
-                {
-                    accelerator: 'CmdOrCtrl+O',
-                    click: () => {
-                        electron.dialog
-                            .showOpenDialog({
-                                filters: [
-                                    {
-                                        extensions: ['h5p'],
-                                        name: 'HTML 5 Package'
-                                    }
-                                ],
-                                properties: ['openFile', 'multiSelections']
-                            })
-                            .then(({ filePaths }) => {
-                                websocket.emit('action', {
-                                    payload: {
-                                        paths: filePaths
-                                    },
-                                    type: 'OPEN_H5P'
-                                });
+                label: i18next.t('lumi:menu.h5peditor.new')
+            },
+            { type: 'separator' } as any,
+            {
+                accelerator: 'CmdOrCtrl+O',
+                click: () => {
+                    electron.dialog
+                        .showOpenDialog({
+                            filters: [
+                                {
+                                    extensions: ['h5p'],
+                                    name: 'HTML 5 Package'
+                                }
+                            ],
+                            properties: ['openFile', 'multiSelections']
+                        })
+                        .then(({ filePaths }) => {
+                            websocket.emit('action', {
+                                payload: {
+                                    paths: filePaths
+                                },
+                                type: 'OPEN_H5P'
                             });
-                    },
-                    label: i18next.t('lumi:menu.h5peditor.open')
-                },
-                { type: 'separator' } as any,
-                {
-                    accelerator: 'CmdOrCtrl+S',
-                    click: () => {
-                        websocket.emit('action', {
-                            type: 'SAVE'
                         });
-                    },
-                    label: i18next.t('lumi:menu.file.save')
                 },
-                {
-                    accelerator: 'Shift+CmdOrCtrl+S',
-                    click: () => {
-                        websocket.emit('action', {
-                            type: 'SAVE_AS'
-                        });
-                    },
-                    label: i18next.t('lumi:menu.file.save_as')
+                label: i18next.t('lumi:menu.h5peditor.open')
+            },
+            { type: 'separator' } as any,
+            {
+                accelerator: 'CmdOrCtrl+S',
+                click: () => {
+                    websocket.emit('action', {
+                        type: 'SAVE'
+                    });
                 },
-                { type: 'separator' } as any,
-                {
-                    click: () => {
-                        websocket.emit('action', {
-                            type: 'EXPORT_AS_HTML'
-                        });
-                    },
-                    label: i18next.t('lumi:menu.file.export')
+                label: i18next.t('lumi:menu.file.save')
+            },
+            {
+                accelerator: 'Shift+CmdOrCtrl+S',
+                click: () => {
+                    websocket.emit('action', {
+                        type: 'SAVE_AS'
+                    });
                 },
-                { type: 'separator' } as any,
-                {
-                    label: i18next.t('lumi:menu.quit'),
-                    role: 'quit'
-                } as any
-            ]
-        },
-        settings.getSettings().allowPrerelease ? runMenu(websocket) : undefined,
-        editMenu(),
-        ...viewMenu(),
-        ...windowMenu(),
-        helpMenu(window, websocket)
-    ].filter((m) => m !== undefined);
+                label: i18next.t('lumi:menu.file.save_as')
+            },
+            settings.getSettings().allowPrerelease !== undefined
+                ? {
+                      label: i18next.t('lumi:menu.run.upload'),
+                      click: () => {
+                          websocket.emit('action', {
+                              payload: {},
+                              type: 'UPLOAD_TO_RUN'
+                          });
+                      }
+                  }
+                : undefined,
+            { type: 'separator' } as any,
+            {
+                click: () => {
+                    websocket.emit('action', {
+                        type: 'EXPORT_AS_HTML'
+                    });
+                },
+                label: i18next.t('lumi:menu.file.export')
+            },
+            { type: 'separator' } as any,
+            {
+                label: i18next.t('lumi:menu.quit'),
+                role: 'quit'
+            } as any
+        ]
+    },
+    editMenu(),
+    ...viewMenu(),
+    ...windowMenu(),
+    helpMenu(window, websocket)
+];
