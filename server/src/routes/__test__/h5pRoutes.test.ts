ace1dc155dcb19f86f9e572d883500e3aeb83941
Sebastian Rettig <sr258@users.noreply.github.com>
2021-09-01 08:18:53 +0200

    test(export): fixed export html tests in Windows

--- server/src/routes/__test__/h5pRoutes.test.ts
+++ server/src/routes/__test__/h5pRoutes.test.ts
@@ -13,7 +13,7 @@ describe('[export h5p as html]: GET /api/v1/h5p/:contentId/html', () => {
 
     beforeAll(async () => {
         const settingsCache = new SettingsCache(
-            path.resolve('test', 'data', 'settings.json')
+            path.join(__dirname, '../../../../test/data/settings.json')
         );
         app = await bootApp(
             {
@@ -25,7 +25,10 @@ describe('[export h5p as html]: GET /api/v1/h5p/:contentId/html', () => {
                     'data',
                     'workingCache'
                 ),
-                settingsFile: path.resolve('test', 'data', 'settings.json')
+                settingsFile: path.join(
+                    __dirname,
+                    '../../../../test/data/settings.json'
+                )
             },
             null,
             settingsCache,
@@ -36,9 +39,13 @@ describe('[export h5p as html]: GET /api/v1/h5p/:contentId/html', () => {
     });
 
     it('exports a html file', async (done) => {
+        const outputPath = path.join(
+            __dirname,
+            '../../../../test/build/test.html'
+        );
         dialog.showSaveDialog = jest.fn(async (c) => ({
             canceled: false,
-            filePath: path.resolve('test', 'build', 'test.html')
+            filePath: path.join(__dirname, '../../../../test/build/test.html')
         }));
 
         const contentId = 740522043;
@@ -48,19 +55,21 @@ describe('[export h5p as html]: GET /api/v1/h5p/:contentId/html', () => {
         );
         expect(res.statusCode).toEqual(200);
 
-        expect(
-            await fsExtra.stat(path.resolve('test', 'build', 'test.html'))
-        ).toBeTruthy();
+        expect(await fsExtra.stat(outputPath)).toBeTruthy();
 
-        fsExtra.removeSync(path.resolve('test', 'build', 'test.html'));
+        fsExtra.removeSync(outputPath);
 
         done();
     }, 30000);
 
     it('appends .html if no extension is defined', async (done) => {
+        const outputPath = path.join(
+            __dirname,
+            '../../../../test/build/test2.html'
+        );
         dialog.showSaveDialog = jest.fn(async (c) => ({
             canceled: false,
-            filePath: path.resolve('test', 'build', 'test2')
+            filePath: outputPath
         }));
 
         const contentId = 740522043;
@@ -70,11 +79,9 @@ describe('[export h5p as html]: GET /api/v1/h5p/:contentId/html', () => {
         );
         expect(res.statusCode).toEqual(200);
 
-        expect(
-            await fsExtra.stat(path.resolve('test', 'build', 'test2.html'))
-        ).toBeTruthy();
+        expect(await fsExtra.stat(outputPath)).toBeTruthy();
 
-        fsExtra.removeSync(path.resolve('test', 'build', 'test2.html'));
+        fsExtra.removeSync(outputPath);
 
         done();
     }, 30000);
