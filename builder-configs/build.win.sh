99f6ac8ab8cf434283119152ca144e9685523df6
Sebastian Rettig <serettig@posteo.de>
2022-03-05 12:19:07 +0100

    refactor: change build order of Windows build

--- builder-configs/build.win.sh
+++ builder-configs/build.win.sh
@@ -1,4 +1,4 @@
-npx --no-install electron-builder --config ./builder-configs/builder.config.clickonce.js --win "$@"
 npx --no-install electron-builder --config ./builder-configs/builder.config.nsis-machine.js --win "$@"
+npx --no-install electron-builder --config ./builder-configs/builder.config.clickonce.js --win "$@"
 npx --no-install electron-builder --config ./builder-configs/builder.config.portable.js --win "$@"
 npx --no-install electron-builder --config ./builder-configs/builder.config.appx.js --win "$@"
