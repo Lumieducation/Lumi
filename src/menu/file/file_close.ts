15006fa7f7b7ae2edd6e2aecc9d22730e7f0016e
JPSchellenberg <jps@Lumi.education>
2025-03-11 20:08:53 +0100

    fix(menu): close now closes the window

--- src/menu/file/file_close.ts
+++ src/menu/file/file_close.ts
@@ -1,9 +1,19 @@
 import { Context } from '../../boot';
 import MenuItem from '../../types/MenuItem';
+import window_close from '../../ops/window_close';
+import window_get_active from '../../ops/window_get_active';
+import window_get_content_id from '../../ops/window_get_content_id';
 
 export default function file_close(ctx: Context): MenuItem {
   return {
     label: ctx.translate('Close'),
-    accelerator: 'CmdOrCtrl+W'
+    accelerator: 'CmdOrCtrl+W',
+    click: async () => {
+      const active_window = await window_get_active();
+      const content_id = await window_get_content_id(active_window);
+
+      active_window.close();
+      await window_close(ctx, content_id);
+    }
   };
 }
