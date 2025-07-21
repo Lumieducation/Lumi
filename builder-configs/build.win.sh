bda6cf1b096bf81227ef5ba145888f15532d715a
Nordln <mail@edwardwatson.co.uk>
2025-07-21 12:03:33 +0200

    Removed redundant configs

--- builder-configs/build.win.sh
+++ builder-configs/build.win.sh
@@ -1,6 +1,2 @@
-npx --no-install electron-builder --config ./builder-configs/builder.config.clickonce.ia32.js --win "$@"
-npx --no-install electron-builder --config ./builder-configs/builder.config.clickonce.arm64.js --win "$@"
-npx --no-install electron-builder --config ./builder-configs/builder.config.nsis-machine.js --win "$@"
 npx --no-install electron-builder --config ./builder-configs/builder.config.clickonce.js --win "$@"
 npx --no-install electron-builder --config ./builder-configs/builder.config.portable.js --win "$@"
-npx --no-install electron-builder --config ./builder-configs/builder.config.appx.js --win "$@"
