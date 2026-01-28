4b06beb13d93a1989187532ad0fd37c59ad86d6d
Oliver Tacke <o.tacke@posteo.de>
2026-01-28 11:09:38 +0100

    chore: Update H5P content type hub endpoints

--- config/default_settings.ts
+++ config/default_settings.ts
@@ -17,8 +17,8 @@ const default_settings = {
   https_proxy: '',
   http_proxy: '',
 
-  hub_content_types_endpoint: 'https://api.h5p.org/v1/content-types/',
-  hub_registration_endpoint: 'https://api.h5p.org/v1/sites'
+  hub_content_types_endpoint: 'https://hub-api.h5p.org/v1/content-types/',
+  hub_registration_endpoint: 'https://hub-api.h5p.org/v1/sites'
 };
 
 export default default_settings;
